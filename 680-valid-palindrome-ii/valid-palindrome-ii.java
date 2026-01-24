dea95259244f0c42bf7ce18cd80239a8ace265fc
Lakshya Tripathi <lakshyatripathiconnect@gmail.com>
2026-01-24 20:14:50 +0530

    Time: 4 ms (99.59%) | Memory: 47.7 MB (58.21%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int l = 0, r = s.length() - 1;
+
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) {
+                return check(s, l + 1, r) || check(s, l, r - 1);
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+    boolean check(String s, int l, int r) {
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
