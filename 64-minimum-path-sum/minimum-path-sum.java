a63ffc2801aab6a735553978a57c31b4caf94f5e
Lakshya Tripathi <lakshyatripathiconnect@gmail.com>
2026-07-17 17:25:29 +0530

    Time: 2 ms (78.30%) | Memory: 50.2 MB (72.31%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -2,26 +2,19 @@ class Solution {
     public int minPathSum(int[][] grid) {
         int m=grid.length;
         int n=grid[0].length;
-        int[][] dp= new int[m][n];
+        int dp[][]=new int[m][n];
         for(int i=0;i<m;i++){
             Arrays.fill(dp[i],-1);
         }
-        return func(m-1,n-1,dp,grid);
+        return help(m-1,n-1,grid,dp);
+        
     }
-   int func(int i, int j, int[][] dp, int[][] grid) {
-    if (i == 0 && j == 0) return grid[i][j];
-    if (i < 0 || j < 0) return Integer.MAX_VALUE;
-    if (dp[i][j] != -1) return dp[i][j];
-    
-    if (i == 0) {
-        return grid[i][j] + func(i, j-1, dp, grid);
+    int help(int i,int j,int[][] grid,int[][] dp){
+        if(i<0 || j<0) return Integer.MAX_VALUE;
+        if(i==0 && j==0) return grid[0][0];
+        if(dp[i][j]!=-1) return dp[i][j];
+       int up=help(i-1,j,grid,dp);
+       int left=help(i,j-1,grid,dp);
+       return dp[i][j]=grid[i][j]+Math.min(up,left);
     }
-    if (j == 0) {
-        return grid[i][j] + func(i-1, j, dp, grid);
-    }
-    
-    int up = grid[i][j] + func(i-1, j, dp, grid);
-    int left = grid[i][j] + func(i, j-1, dp, grid);
-    return dp[i][j] = Math.min(up, left);
-}
 }
