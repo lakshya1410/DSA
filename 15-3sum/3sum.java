169cb4e7247b5f171e9c8050254657f03cd74c09
Lakshya Tripathi <lakshyatripathiconnect@gmail.com>
2026-08-26 22:44:02 +0530

    Time: 32 ms (77.30%) | Memory: 60.6 MB (5.31%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,31 +1,48 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        int n=nums.length;
-        List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
-
+        int n=nums.length;
+        
+        List<List<Integer>> ans = new ArrayList<>();
+        if(n<3) return ans;
         for(int i=0;i<n;i++){
-
-            if(i>0 && nums[i]==nums[i-1]) continue;
-            int j=i+1;
-            int k=n-1;
-
-            while(j<k){
-                int sum=nums[i]+nums[j]+nums[k];
-                if(sum<0){
-                    j++;
-                }
-                else if(sum>0){
-                    k--;
-                }
-                else{
-                    ans.add(Arrays.asList(nums[i],nums[j],nums[k]));
-                    j++;
-                    k--;
-
-                    while(j<k && nums[j-1]==nums[j]) j++;
-                }
+            if(i!=0){
+                if(nums[i]==nums[i-1]) continue;
             }
+            int target=-nums[i];
+            int left=i+1;
+            int right=n-1;
+            while(left<right){
+                 int currsem=nums[left]+nums[right];
+                 if(currsem>target){
+                    right--;
+
+                 }
+                 else if(currsem<target){
+                    left++;
+                 }
+                
+               
+               else{
+                while(left<right &&nums[left]==nums[left+1]) {
+                    left++;
+                    
+                }
+                while(left<right && nums[right]==nums[right-1]){
+                    right--;
+                    
+                }
+                    ArrayList<Integer> curr= new ArrayList<>();
+                    curr.add(nums[i]);
+                    curr.add(nums[left]);
+                    curr.add(nums[right]);
+                    ans.add(curr);
+                    left++;
+                    right--;
+                }
+
+            }
+            
         }
         return ans;
     }
