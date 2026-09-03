5db7859d4204ebc441c30e8023a75b404990e67b
Lakshya Tripathi <lakshyatripathiconnect@gmail.com>
2026-09-03 13:55:32 +0530

    Time: 1091 ms (5.00%) | Memory: 46.9 MB (5.04%) - LeetSync

---
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        return solve(s,p);
+    }
+    boolean solve(String s , String p ){
+          if (p.length() == 0) {
+            return s.length() == 0;
+        }
+        boolean first_char_matched=false;
+      if (s.length() > 0 &&
+    (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.')){
+            first_char_matched=true;
+        } 
+
+        if(p.length() >= 2 && p.charAt(1)=='*'){
+            boolean not_take=solve(s,p.substring(2));
+            boolean take=first_char_matched && solve(s.substring(1),p);
+            return not_take || take;
+        }
+           else { if (!first_char_matched) { return false; } return solve(s.substring(1), p.substring(1)); }
+        
+    }
+}
