7fda1a594d6b7b7d4742fd9808c36f9c5fe9fd6d
Lakshya Tripathi <lakshyatripathiconnect@gmail.com>
2026-09-19 14:26:18 +0530

    Time: 1 ms (87.96%) | Memory: 45.7 MB (15.99%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,22 +1,21 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> cur = new ArrayList<>();
-        boolean freq[] = new boolean[nums.length];
-        help(nums,ans,cur,freq);
+        List<Integer> curr = new ArrayList<>();
+        help(ans,curr,nums,new boolean[nums.length]);
         return ans;
-
     }
-    void help(int[] nums,List<List<Integer>> ans ,List<Integer> cur,   boolean freq[]   ){
-        if(cur.size()==nums.length) ans.add(new ArrayList<>(cur));
+    void help(List<List<Integer>> ans , List<Integer> curr,int [] nums,boolean[] freq){
+        if(curr.size()==nums.length) ans.add(new ArrayList<>(curr));
         for(int i=0;i<nums.length;i++){
             if(!freq[i]){
                 freq[i]=true;
-                cur.add(nums[i]);
-                help(nums,ans,cur,freq);
-                cur.remove(cur.size()-1);
+                curr.add(nums[i]);
+                help(ans,curr,nums,freq);
+                curr.remove(curr.size()-1);
                 freq[i]=false;
             }
         }
+        
     }
 }
