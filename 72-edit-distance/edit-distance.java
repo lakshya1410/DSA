b99fd469c5cfa6758ede5d9f97d60fef24055b58
Lakshya Tripathi <lakshyatripathiconnect@gmail.com>
2026-09-21 14:48:35 +0530

    Time: 3 ms (98.10%) | Memory: 47.3 MB (18.15%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,29 +1,25 @@
 class Solution {
-    int [][] dp;
     public int minDistance(String w1, String w2) {
-        dp = new int[w1.length() + 1][w2.length() + 1];
-        for (int i = 0; i <= w1.length(); i++) {
-            for (int j = 0; j <= w2.length(); j++) {
-                dp[i][j] = -1;
-            }
+        int n= w1.length();
+        int m=w2.length();
+        int [][] dp = new int[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            Arrays.fill(dp[i],-1);
         }
-        return solve(w1,w2,w1.length(),w2.length());
-        
+        return help(w1,w2,n,m,dp);
     }
-    int solve(String w1,String w2,int i,int j){
+    int help(String w1,String w2,int i,int j,int[][] dp){
         if(i==0) return j;
         if(j==0) return i;
-
-        if (dp[i][j] != -1) return dp[i][j];
+        if(dp[i][j]!=-1) return dp[i][j];
 
         if(w1.charAt(i-1)==w2.charAt(j-1)){
-            return solve(w1,w2,i-1,j-1);
+            return dp[i][j]=help(w1,w2,i-1,j-1,dp);
         }
-        int insert=solve(w1,w2,i,j-1);
-        int delete=solve(w1,w2,i-1,j);
-        int replace=solve(w1,w2,i-1,j-1);
+        int insert=help(w1,w2,i,j-1,dp);
+        int delete=help(w1,w2,i-1,j,dp);
+        int replace=help(w1,w2,i-1,j-1,dp);
 
-       return  dp[i][j]= 1+Math.min(insert,Math.min(delete,replace));
-
+        return dp[i][j]= 1+Math.min(insert,Math.min(delete,replace));
     }
 }
